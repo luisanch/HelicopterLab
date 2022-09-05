37c8d7419ba617f5c320740fda45ea77e69a1c27
luisanch <luissanchezmtn@gmail.com>
2022-09-05 19:03:21 +0200

    Update pole_placement.m
    fancy ui

--- pole_placement.m
+++ pole_placement.m
@@ -2,7 +2,7 @@ clc
 clearvars
 init_heli_1_2
 
-syms k_pp k_pd s xi
+syms k_pp k_pd s xi 
 
 %Natural frequency
 om_n = pi;
@@ -17,7 +17,32 @@ k_pd = 2*xi*om_n / k1;
 sys = tf([k1*k_pp],[1 k1*k_pd k1*k_pp]);
 
 %Plots
-subplot(2,1,1)
-step(sys)
-subplot(2,1,2)
-impulse(sys)
+f = figure;
+ax = axes('Parent',f,'position',[0.13 0.39  0.77 0.54]);
+h = stepplot(ax,sys);
+setoptions(h,'XLim',[0,10],'YLim',[0,2]);
+
+b = uicontrol('Parent',f,'Style','slider','Position',[81,54,419,23],...
+              'value',xi, 'min',0, 'max',2);
+bgcolor = f.Color;
+bl1 = uicontrol('Parent',f,'Style','text','Position',[50,54,23,23],...
+                'String','0','BackgroundColor',bgcolor);
+bl2 = uicontrol('Parent',f,'Style','text','Position',[500,54,23,23],...
+                'String','1','BackgroundColor',bgcolor);
+bl3 = uicontrol('Parent',f,'Style','text','Position',[240,25,100,23],...
+                'String','Damping Ratio','BackgroundColor',bgcolor);
+
+
+
+c = uicontrol('Parent',f,'Style','slider','Position',[81,100,419,23],...
+              'value',om_n, 'min',0, 'max',4*pi); 
+cl1 = uicontrol('Parent',f,'Style','text','Position',[50,100,23,23],...
+                'String','0','BackgroundColor',bgcolor);
+cl2 = uicontrol('Parent',f,'Style','text','Position',[500,100,23,23],...
+                'String','4 pi','BackgroundColor',bgcolor);
+cl3 = uicontrol('Parent',f,'Style','text','Position',[240,75,100,23],...
+                'String','Natural Frequency','BackgroundColor',bgcolor);
+
+c.Callback = @(ces,ced) updateSystem(h,tf([k1*((c.Value)^2 / k1)],[1 k1*(2*b.Value*(c.Value) / k1) k1*((c.Value)^2 / k1)])); 
+b.Callback = @(ces,ced) updateSystem(h,tf([k1*((c.Value)^2 / k1)],[1 k1*(2*b.Value*(c.Value) / k1) k1*((c.Value)^2 / k1)]));
+ 
